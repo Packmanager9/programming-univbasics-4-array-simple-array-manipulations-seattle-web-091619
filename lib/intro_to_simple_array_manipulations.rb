1e217cdc9afa224d5f91c1931e107b7f432347b2
Cole Ditzler <colejditzler@gmail.com>
2019-09-11 20:55:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,7 +35,8 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
-  array.delete(string)
+  a = array.include?(string).index
+  array.delete(a)
 end
 
 def using_delete_at (array, integer)
