ba76de724e539aa662682495b5ce7cfb0bf11629
Cole Ditzler <colejditzler@gmail.com>
2019-09-11 20:48:47 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+def using_push(array, string) do
+  array.push(string)
+end
+
+def using_unshift (array)do
+  array.unshift
+end
+
+def using_pop (array)do
+  array.pop
+end
+
+def using_with_args(array) do
+  array.
+end
+
+def using_shift(array) do
+  array.
+end
+
+def using_concat(array1, array2) do
+
+end
+
+def using_insert (array, element)do
+  array[4].insert(element)
+end
+
+def using_uniq (array)do
+  array.uniq
+end
+
+def using_flatten(array) do
+  array.flatten
+end
+
+def using_delete(array, string) do
+  array.delete(string)
+end
+
+def using_delete_at (array, integer) do
+  array[integer].delete
+end
